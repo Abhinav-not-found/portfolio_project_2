766fea0b5d5e257b81e9061dc2616c6d8c4a2bbb
Abhinav-not-found <abhinavisgreat18@gmail.com>
2023-08-20 01:29:55 +0530

    added my folder files

--- src/Components/Applications/My_folder/My_folder.js
+++ src/Components/Applications/My_folder/My_folder.js
@@ -1,7 +1,42 @@
 import React from 'react'
-
+import pdf_icon from './Images/pdf_icon.png'
+import './My_folder.css'
 export default function My_folder() {
   return (
-    <div>My_folder</div>
+    <div className='folder-wrapper'>
+        <div className="folder-left"></div>
+        <div className="folder-right">
+          <div className="folder-elements">
+            <img src={pdf_icon} alt="" />
+            <p className='f-name'>CV</p>
+            <p className='f-file-type'>Microsoft Edge PD...</p>
+            <p className='f-size'>121KB</p>
+          </div>
+          <div className="folder-elements">
+            <img src={pdf_icon} alt="" />
+            <p className='f-name'>name</p>
+            <p className='f-file-type'>file-type</p>
+            <p className='f-size'>size</p>
+          </div>
+          <div className="folder-elements">
+            <img src={pdf_icon} alt="" />
+            <p className='f-name'>name</p>
+            <p className='f-file-type'>file-type</p>
+            <p className='f-size'>size</p>
+          </div>
+          <div className="folder-elements">
+            <img src={pdf_icon} alt="" />
+            <p className='f-name'>name</p>
+            <p className='f-file-type'>file-type</p>
+            <p className='f-size'>size</p>
+          </div>
+          <div className="folder-elements">
+            <img src={pdf_icon} alt="" />
+            <p className='f-name'>name</p>
+            <p className='f-file-type'>file-type</p>
+            <p className='f-size'>size</p>
+          </div>
+        </div>
+    </div>
   )
 }
